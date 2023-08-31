03010ada11786c575fd904380203be8f9feac49a
bala1802 <bala1802@live.com>
2023-08-31 08:56:33 +0530

    added architecture for the dataset.py

--- Sentiment_Analysis_Architecture/dataset.py
+++ Sentiment_Analysis_Architecture/dataset.py
@@ -4,13 +4,27 @@ import pytorch_lightning as pl
 
 class CustomDataset(Dataset):
     def __init__(self, texts, labels, tokenizer, max_length):
-        pass
-
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+    
     def __len__(self):
         return len(self.texts)
     
     def __getitem__(self, idx):
-        pass
+        text = self.texts[idx]
+        label = self.labels[idx]
+        encoded = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            padding="max-length",
+            return_tensors="pt"
+        )
+        input_ids = encoded["input_ids"].squeeze()
+        attention_mask = encoded["attention_mask"].squeeze()
+        return input_ids, attention_mask, label
 
 class SentimentDataModule(pl.LightningDataModule):
     def __init__(self):
