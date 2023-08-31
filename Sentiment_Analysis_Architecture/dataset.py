ca9cd2dc88119a3acee91e91d15ed06f20ee1d86
bala1802 <bala1802@live.com>
2023-08-31 09:11:24 +0530

    completed writing dataset.py

--- Sentiment_Analysis_Architecture/dataset.py
+++ Sentiment_Analysis_Architecture/dataset.py
@@ -22,19 +22,27 @@ class CustomDataset(Dataset):
             padding="max-length",
             return_tensors="pt"
         )
-        input_ids = encoded["input_ids"].squeeze()
-        attention_mask = encoded["attention_mask"].squeeze()
+        input_ids = encoded["input_ids"].squeeze() #Visualize the shape of input_ids before and after squeezing
+        attention_mask = encoded["attention_mask"].squeeze() #Visualize the shape of attention_mask before and after squeezing
         return input_ids, attention_mask, label
 
 class SentimentDataModule(pl.LightningDataModule):
-    def __init__(self):
-        pass
+    def __init__(self, train_texts, train_labels, val_texts, val_labels, tokenizer, batch_size, max_length):
+        super(SentimentDataModule, self).__init__()
+        self.train_texts = train_texts
+        self.train_labels = train_labels
+        self.val_texts = val_texts
+        self.val_labels = val_labels
+        self.tokenizer = tokenizer
+        self.batch_size = batch_size
+        self.max_length = max_length
     
     def setup(self):
-        pass
+        self.train_dataset = CustomDataset(self.train_texts, self.train_labels, self.tokenizer, self.max_length)
+        self.val_dataset = CustomDataset(self.val_texts, self.val_labels, self.tokenizer, self.max_length)    
 
     def train_dataloader(self):
-        pass
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self):
-        pass
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
